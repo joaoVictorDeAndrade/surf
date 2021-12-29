90efae2fa0630d15be851378b13f82a912b6a0f2
joao <joao.andrade@conpec.com.br>
2021-12-28 23:11:27 -0300

    Fazendo sessao destaque

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
+import FeaturedContent from "./components/FeaturedContent";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <FeaturedContent />
     </div>
   )
 }
---
+++ src/components/FeaturedContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./FeaturedContent.module.css";
+
+export default function FeaturedContent() {
+  return (
+    <section className={styles.featured}>
+      <h1>Escola de Surf</h1>
+      <a href="">Matricule-se</a>
+    </section>
+  );
+}
